25ac2b7bd2f3f8006a1ffbaf9059aaeb45740d50
unknown <goelaaditya2003@gmail.com>
2024-04-21 02:10:56 +0530

    Changes

--- src/app/api/sign-out/[[...sign-out]]/page.tsx
+++ src/app/api/sign-out/[[...sign-out]]/page.tsx
@@ -1,15 +1,24 @@
 'use client';
-import { useClerk } from "@clerk/clerk-react";
-import { useRouter } from 'next/navigation'
- 
-const SignOutButton = () => {
+import { useClerk } from "@clerk/nextjs";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+
+export default function Page() {
   const { signOut } = useClerk();
-  const router = useRouter()
- 
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleSignOut = async () => {
+      await signOut();
+      router.push("/");
+    };
+
+    handleSignOut();
+  }, [signOut, router]);
+
   return (
-    // Clicking on this button will sign out a user and reroute them to the "/" (home) page.
-    <button onClick={() => signOut(() => router.push("/"))}>
-      Sign out
-    </button>
+    <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+      <p>Signing out...</p>
+    </div>
   );
-};
+}
