a75d2359f41cbae22c45c86fd2170df62641a260
unknown <goelaaditya2003@gmail.com>
2024-04-23 23:21:05 +0530

    Changes

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -25,7 +25,7 @@ const DashboardPage = async (props: Props) => {
 
   return (
     <>
-      <div className={`min-h-screen ${isDarkTheme ? 'dark' : ''}`}>
+      <div className='min-h-screen'>
         <Navbar/>
         <div className="max-w-7xl mx-auto p-10">
           <div className="h-14"></div>
