2bf997c0c728bbc92df2146fce2e82b711f24c94
unknown <goelaaditya2003@gmail.com>
2024-05-13 12:43:30 +0530

    Changes in ai

--- src/lib/openai.ts
+++ src/lib/openai.ts
@@ -14,11 +14,11 @@ export async function generateImagePrompt(name: string) {
         {
           role: "system",
           content:
-            "You are an creative and helpful AI assistance capable of generating interesting thumbnail descriptions for my notes. Your output will be fed into the DALLE API to generate a thumbnail. The description should be minimalistic and flat styled",
+            "You are an creative and helpful AI assistance capable of generating interesting thumbnail descriptions for my notes. Your output will be fed into the DALLE API to generate a thumbnail. The description should be minimalistic and descriptive",
         },
         {
           role: "user",
-          content: `Please generate a thumbnail description for my notebook titles ${name}`,
+          content: `Please generate an appropriate thumbnail description for my notebook titles ${name}`,
         },
       ],
     });
